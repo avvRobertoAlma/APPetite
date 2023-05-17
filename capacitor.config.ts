a7c7838c88ffbd87ad989fb33c2716e3c9e6a0da
Roberto <roberto.alma@kbl-law.com>
2023-05-17 23:25:47 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'APPetite',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
