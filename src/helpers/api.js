638539b15446a722d27995083da3e1d1d3c82c7a
fanforfun <fanforfun9@gmail.com>
2023-06-20 11:41:58 +0200

    Resolve issues on nutriscore, implement new products for different animals, add interactions on scanned products at animal switch

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,41 +1,41 @@
-import pets from '../mocks/ui.json'
-import nutriscoreRankings from '../mocks/nutriscore.json'
+import pets from "../mocks/ui.json";
+import nutriscoreRankings from "../mocks/nutriscore.json";
 
-
-export const Api =  {
-    getPets(){
-        return pets
-    },
-    getForbiddenFoods(type){
-        console.log(type)
-        let pet = pets.find((el)=>{
-            return el.tipo == type
-        })
-        return pet.alimentiVietati
-    },
-    getPetTypes(){
-        let types = []
-        for (let i=0; i<pets.length; i++){
-            console.log(pets[i])
-            types.push(pets[i].tipo)
-        }
-        console.log(types)
-        return types
-    },
-    getPetRaces(type){
-        console.log(type)
-        let pet = pets.find((el)=>{
-            return el.tipo == type
-        })
-        return pet.razze
-    },
-    getNutriscoreRanking(barcode){
-        const product = nutriscoreRankings.find(function(el){
-            return el.barcode == barcode
-        })
-        if (!product){
-            return null
-        } 
-        return product
+export const Api = {
+  getPets() {
+    return pets;
+  },
+  getForbiddenFoods(type) {
+    console.log(type);
+    let pet = pets.find((el) => {
+      return el.tipo == type;
+    });
+    return pet.alimentiVietati;
+  },
+  getPetTypes() {
+    let types = [];
+    for (let i = 0; i < pets.length; i++) {
+      console.log(pets[i]);
+      types.push(pets[i].tipo);
     }
-}
+    console.log(types);
+    return types;
+  },
+  getPetRaces(type) {
+    console.log(type);
+    let pet = pets.find((el) => {
+      return el.tipo == type;
+    });
+    return pet.razze;
+  },
+  getNutriscoreRanking(barcode) {
+    const product = nutriscoreRankings.find(function (el) {
+      return el.barcode == barcode;
+    });
+    console.log(product);
+    if (!product) {
+      return null;
+    }
+    return product;
+  },
+};
--- src/pages/NutriscorePage.vue
+++ src/pages/NutriscorePage.vue
@@ -1,87 +1,81 @@
 <template>
   <default-layout pageTitle="Nutriscore" pageDefaultBackLink="/home">
     <div v-if="isScannerOpen">
-      <ion-content class="ion-padding">
-        <h3 style="margin-top: 10% !important; text-align: center">
+      <ion-content class="ion-padding" style="position: absolute">
+        <h3 style="margin-top: 10% !important; text-align: center !important">
           Scansiona il codice a barre del prodotto per valutarlo
         </h3>
+        <ion-img src="scanner.jpeg" @click="loadNutriscoreRanking()" />
       </ion-content>
-      <ion-img src="scanner.jpeg" @click="loadNutriscoreRanking('sample')" />
     </div>
     <div v-if="isRankingOpen">
+      <!-- Sample dog -->
       <ion-grid>
-        <ion-row>
+        <ion-row class="ion-align-items-center">
           <ion-col>
             <ion-img :src="rankings.img" />
           </ion-col>
           <ion-col>
-            <h3 style="text-align: center">{{ rankings.product }}</h3>
+            <h3 style="text-align: center !important">{{ rankings.product }}</h3>
           </ion-col>
         </ion-row>
         <ion-row>
           <ion-col></ion-col>
-          <ion-col
-            size="auto"
-            size-lg="auto"
-            size-md="auto"
-            size-sm="auto"
-            size-xl="auto"
-            size-xs="auto"
-            style="width: 50%"
-          >
-            <h1 :class="nutriscoreClass">{{ rankings.points }}/100</h1>
+          <ion-col size="auto" size-lg="auto" size-md="auto" size-sm="auto" size-xl="auto" size-xs="auto"
+            style="width: 50%">
+            <h1 v-if="this.$store.getters.getActivePet.type == rankings.pet" :class="nutriscoreClass">{{ rankings.points
+            }}/100
+            </h1>
+            <h1 v-else class="nutriscore-points-red">0/100</h1>
           </ion-col>
           <ion-col></ion-col>
         </ion-row>
-        <ion-note>Elementi positivi</ion-note>
-        <ion-row>
-          <ion-col>
-            <ion-list>
-              <ion-item
-                expand="block"
-                v-for="positive in rankings.positives"
-                @click="setSelectedPositive(positive)"
-              >
-                <ion-label>{{ positive.name }}</ion-label>
-                <ion-icon
-                  :icon="informationCircle"
-                  style="color: #eb8500"
-                  slot="end"
-                ></ion-icon>
-              </ion-item>
-            </ion-list>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <ion-col>
-            <ion-note>Ingredienti</ion-note>
-          </ion-col>
-          <ion-col push="3">
-            <a @click="viewIngredients">Vedi tutto</a>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <ion-col>
-            <ion-list>
-              <ion-item
-                expand="block"
-                v-for="item in rankings.overall"
-                @click="setSelectedOverall(item)"
-              >
-                <ion-label>{{ item.description }}</ion-label>
-                <ion-icon
-                  :icon="informationCircle"
-                  style="color: #eb8500"
-                  slot="end"
-                ></ion-icon>
-              </ion-item>
-            </ion-list>
-          </ion-col>
-        </ion-row>
+        <!-- Product matches with animal -->
+        <div v-if="this.$store.getters.getActivePet.type == rankings.pet">
+          <ion-note>Elementi positivi</ion-note>
+          <ion-row>
+            <ion-col>
+              <ion-list>
+                <ion-item expand="block" v-for="positive in rankings.positives">
+                  <ion-label>{{ positive.name }}</ion-label>
+                  <ion-icon :icon="informationCircle" style="color: #eb8500" @click="setSelectedPositive(positive)"
+                    slot="end"></ion-icon>
+                </ion-item>
+              </ion-list>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <ion-note>Ingredienti</ion-note>
+            </ion-col>
+            <ion-col push="3">
+              <a @click="viewIngredients">Vedi tutto</a>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <ion-list>
+                <ion-item expand="block" v-for="item in rankings.overall">
+                  <ion-label>{{ item.description }}</ion-label>
+                  <ion-icon :icon="informationCircle" style="color: #eb8500" @click="setSelectedOverall(item)"
+                    slot="end"></ion-icon>
+                </ion-item>
+              </ion-list>
+            </ion-col>
+          </ion-row>
+        </div>
+        <!-- Product doesn't match with animal -->
+        <div v-else>
+          <ion-row class="ion-justify-content-center">
+            <span id="wrong-product">Il prodotto non è indicato per questa specie
+              animale</span>
+          </ion-row>
+        </div>
       </ion-grid>
+      <!-- End sample dog -->
     </div>
     <!-- Modal for ingredients -->
-    <ion-modal :is-open="isOpenModalIngredients">
+    <ion-modal :is-open="isOpenModalIngredients" style="--height: 100%;">
       <ion-header>
         <ion-toolbar>
           <ion-title>Dettaglio Ingredienti</ion-title>
@@ -96,30 +90,17 @@
             <ion-label>
               {{ element.description }}
             </ion-label>
-            <ion-icon
-              :icon="alertCircleSharp"
-              v-if="element.risk == 'medium'"
-              style="color: rgb(235, 235, 0)"
-              slot="end"
-            ></ion-icon>
-            <ion-icon
-              :icon="warningSharp"
-              v-if="element.risk == 'high'"
-              style="color: red"
-              slot="end"
-            ></ion-icon>
+            <ion-icon :icon="alertCircleSharp" v-if="element.risk == 'medium'" style="color: rgb(200, 200, 0)"
+              slot="end"></ion-icon>
+            <ion-icon :icon="warningSharp" v-if="element.risk == 'high'" style="color: red" slot="end"></ion-icon>
           </ion-item>
         </ion-list>
       </ion-content>
     </ion-modal>
     <!-- end Modal for ingredients -->
     <!-- Modal for characteristics -->
-    <ion-modal
-      :isOpen="isOpenModalCharacteristics"
-      @ionModalDidDismiss="closeModalCharacteristics"
-      :initial-breakpoint="1"
-      :breakpoints="[0, 1]"
-    >
+    <ion-modal :isOpen="isOpenModalCharacteristics" @ionModalDidDismiss="closeModalCharacteristics"
+      :initial-breakpoint="1" :breakpoints="[0, 1]" id="top-down">
       <div class="block">
         <ion-grid>
           <ion-row>
@@ -149,12 +130,8 @@
     </ion-modal>
     <!-- end Modal for characteristics -->
     <!-- Modal for overall -->
-    <ion-modal
-      :isOpen="isOpenModalOverall"
-      @ionModalDidDismiss="closeModalOverall"
-      :initial-breakpoint="1"
-      :breakpoints="[0, 0.5, 1]"
-    >
+    <ion-modal :isOpen="isOpenModalOverall" @ionModalDidDismiss="closeModalOverall" :initial-breakpoint="1"
+      :breakpoints="[0, 1]" id="top-down">
       <div class="block">
         <ion-grid>
           <ion-row>
@@ -168,22 +145,10 @@
             <ion-col></ion-col>
             <ion-col size="auto">
               <span style="text-align: center; vertical-align: middle">
-                <ion-icon
-                  v-if="selectedOverall.risk == 'rischio alto'"
-                  :icon="ellipseSharp"
-                  style="color: red"
-                ></ion-icon>
-                <ion-icon
-                  v-if="selectedOverall.risk == 'rischio medio'"
-                  :icon="ellipseSharp"
-                  style="color: orange"
-                ></ion-icon>
-                <ion-icon
-                  v-if="selectedOverall.risk == 'rischio basso'"
-                  :icon="ellipseSharp"
-                  style="color: yellow"
-                ></ion-icon>
-                {{ selectedOverall.risk }}
+                <ion-icon v-if="selectedOverall.risk == 'alto'" :icon="ellipseSharp" style="color: red"></ion-icon>
+                <ion-icon v-if="selectedOverall.risk == 'medio'" :icon="ellipseSharp" style="color: orange"></ion-icon>
+                <ion-icon v-if="selectedOverall.risk == 'basso'" :icon="ellipseSharp" style="color: yellow"></ion-icon>
+                rischio {{ selectedOverall.risk }}
               </span>
             </ion-col>
             <ion-col></ion-col>
@@ -282,7 +247,11 @@ export default {
     },
   },
   methods: {
-    async loadNutriscoreRanking(barcode) {
+    async loadNutriscoreRanking() {
+      var barcode;
+      if (this.$store.getters.getActivePet.type == "cane") { barcode = 'sample-dog' }
+      else if (this.$store.getters.getActivePet.type == "gatto") { barcode = 'sample-cat' }
+      else if (this.$store.getters.getActivePet.type == "tartaruga") { barcode = 'sample-turtle' }
       this.rankings = await Api.getNutriscoreRanking(barcode);
       this.isScannerOpen = false;
       this.isRankingOpen = true;
@@ -299,6 +268,12 @@ export default {
     closeModalCharacteristics() {
       this.isOpenModalCharacteristics = false;
     },
+    viewOverall() {
+      this.isOpenModalOverall = true;
+    },
+    closeModalOverall() {
+      this.isOpenModalOverall = false;
+    },
     setSelectedPositive(positive) {
       console.log(positive);
       try {
@@ -307,12 +282,6 @@ export default {
       } catch (err) {
         console.log(err);
       }
-    },
-    viewOverall() {
-      this.isOpenModalOverall = true;
-    },
-    closeModalOverall() {
-      this.isOpenModalOverall = false;
     },
     setSelectedOverall(item) {
       console.log(item);
@@ -388,7 +357,16 @@ ion-item {
   justify-content: center;
 }
 
-ion-modal {
+#top-down {
   --height: auto;
+}
+
+#wrong-product {
+  text-align: center;
+  font-weight: bold;
+  text-decoration: underline;
+  color: red;
+  --ion-grid-column-padding: 5px;
+  margin-top: 10px;
 }
 </style>
