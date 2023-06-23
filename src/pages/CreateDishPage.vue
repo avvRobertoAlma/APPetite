167b4bcab1727dae2e31de56295856b2011cc6bc
Roberto <roberto.alma@kbl-law.com>
2023-06-23 23:18:12 +0200

    dataDirectory

--- src/pages/CreateDishPage.vue
+++ src/pages/CreateDishPage.vue
@@ -159,7 +159,7 @@ export default {
         const base64 = await PDFGenerator.fromData(html, options)
         var fileName = this.activePet.name + '_dish.pdf';
         // var folderpath = "file:///storage/emulated/0/Download/";
-        var folderpath = cordova.file.externalRootDirectory + "Download/";
+        var folderpath = cordova.file.dataDirectory + "Download/";
         var contentType = "application/pdf";
         Services.savebase64AsPDF(folderpath, fileName, base64, contentType);
       } catch (err){
