63653ba03b8174318c9dc20a809c27b38b3257ce
FanForFun <fanforfun9@gmail.com>
2023-06-24 00:57:09 +0200

    Add logo for apk

--- start-android.sh
+++ start-android.sh
@@ -1,4 +1,5 @@
 ionic build
 npx cap sync
+npx @capacitor/assets generate --iconBackgroundColor '#eeeeee' --iconBackgroundColorDark '#222222' --splashBackgroundColor '#eeeeee' --splashBackgroundColorDark '#111111'
 npx cap copy
 ionic capacitor open android
